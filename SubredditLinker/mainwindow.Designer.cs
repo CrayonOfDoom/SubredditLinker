ab0939a33f7279c107c8020e26e7705da8126cc5
Robin King <CrayonOfDoom@users.noreply.github.com>
2016-12-28 09:43:49 -0700

    Initial Commit
    Base version of SubredditLinker.

---
+++ SubredditLinker/mainwindow.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace SubredditLinker
+{
+    partial class mainwindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSubreddit = new System.Windows.Forms.TextBox();
+            this.buttonAnalyze = new System.Windows.Forms.Button();
+            this.tableLayoutPanelOuter = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanelInner = new System.Windows.Forms.TableLayoutPanel();
+            this.progressBarMain = new System.Windows.Forms.ProgressBar();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.treeViewResults = new System.Windows.Forms.TreeView();
+            this.treeViewSub = new System.Windows.Forms.TreeView();
+            this.tableLayoutPanelOuter.SuspendLayout();
+            this.tableLayoutPanelInner.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxSubreddit
+            // 
+            this.textBoxSubreddit.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBoxSubreddit.Location = new System.Drawing.Point(203, 4);
+            this.textBoxSubreddit.Name = "textBoxSubreddit";
+            this.textBoxSubreddit.Size = new System.Drawing.Size(391, 20);
+            this.textBoxSubreddit.TabIndex = 0;
+            // 
+            // buttonAnalyze
+            // 
+            this.buttonAnalyze.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.buttonAnalyze.Location = new System.Drawing.Point(600, 3);
+            this.buttonAnalyze.Name = "buttonAnalyze";
+            this.buttonAnalyze.Size = new System.Drawing.Size(134, 23);
+            this.buttonAnalyze.TabIndex = 1;
+            this.buttonAnalyze.Text = "Analyze Mods";
+            this.buttonAnalyze.UseVisualStyleBackColor = true;
+            this.buttonAnalyze.Click += new System.EventHandler(this.buttonAnalyze_Click);
+            // 
+            // tableLayoutPanelOuter
+            // 
+            this.tableLayoutPanelOuter.ColumnCount = 1;
+            this.tableLayoutPanelOuter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelOuter.Controls.Add(this.tableLayoutPanelInner, 0, 1);
+            this.tableLayoutPanelOuter.Controls.Add(this.tabControl1, 0, 0);
+            this.tableLayoutPanelOuter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelOuter.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelOuter.Name = "tableLayoutPanelOuter";
+            this.tableLayoutPanelOuter.RowCount = 2;
+            this.tableLayoutPanelOuter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelOuter.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanelOuter.Size = new System.Drawing.Size(743, 438);
+            this.tableLayoutPanelOuter.TabIndex = 2;
+            // 
+            // tableLayoutPanelInner
+            // 
+            this.tableLayoutPanelInner.ColumnCount = 3;
+            this.tableLayoutPanelInner.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanelInner.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelInner.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanelInner.Controls.Add(this.buttonAnalyze, 2, 0);
+            this.tableLayoutPanelInner.Controls.Add(this.textBoxSubreddit, 1, 0);
+            this.tableLayoutPanelInner.Controls.Add(this.progressBarMain, 0, 0);
+            this.tableLayoutPanelInner.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelInner.Location = new System.Drawing.Point(3, 408);
+            this.tableLayoutPanelInner.Name = "tableLayoutPanelInner";
+            this.tableLayoutPanelInner.RowCount = 1;
+            this.tableLayoutPanelInner.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanelInner.Size = new System.Drawing.Size(737, 27);
+            this.tableLayoutPanelInner.TabIndex = 0;
+            // 
+            // progressBarMain
+            // 
+            this.progressBarMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBarMain.Location = new System.Drawing.Point(3, 3);
+            this.progressBarMain.Name = "progressBarMain";
+            this.progressBarMain.Size = new System.Drawing.Size(194, 23);
+            this.progressBarMain.TabIndex = 2;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(737, 399);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.treeViewResults);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(729, 373);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "By User";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.treeViewSub);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(729, 373);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "By Subreddit";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // treeViewResults
+            // 
+            this.treeViewResults.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeViewResults.Location = new System.Drawing.Point(3, 3);
+            this.treeViewResults.Name = "treeViewResults";
+            this.treeViewResults.Size = new System.Drawing.Size(723, 367);
+            this.treeViewResults.TabIndex = 0;
+            // 
+            // treeViewSub
+            // 
+            this.treeViewSub.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeViewSub.Location = new System.Drawing.Point(3, 3);
+            this.treeViewSub.Name = "treeViewSub";
+            this.treeViewSub.Size = new System.Drawing.Size(723, 367);
+            this.treeViewSub.TabIndex = 0;
+            // 
+            // mainwindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 438);
+            this.Controls.Add(this.tableLayoutPanelOuter);
+            this.Name = "mainwindow";
+            this.Text = "SubredditLinker";
+            this.tableLayoutPanelOuter.ResumeLayout(false);
+            this.tableLayoutPanelInner.ResumeLayout(false);
+            this.tableLayoutPanelInner.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSubreddit;
+        private System.Windows.Forms.Button buttonAnalyze;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelOuter;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelInner;
+        private System.Windows.Forms.ProgressBar progressBarMain;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TreeView treeViewResults;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TreeView treeViewSub;
+    }
+}
+
---
+++ SubredditLinker/mainwindow.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RedditSharp;
+using HtmlAgilityPack;
+using System.Threading;
+
+namespace SubredditLinker
+{
+    public partial class mainwindow : Form
+    {
+        // Reddit access info
+        public Reddit mainReddit;
+        public HtmlWeb web;
+
+        // Main subreddit info
+        public string mainSubreddit;
+        public bool isValid;
+        public IEnumerable<ModeratorUser> moderators;
+
+        // Structure for holding all the subs that the moderators mod
+        public List<string> modNames;
+        public List<List<string>> modSubsList;
+
+        // Misc needed things
+        public int barNumber;
+
+        public mainwindow()
+        {
+            InitializeComponent();
+
+            // Accept button should be set
+            this.AcceptButton = buttonAnalyze;
+
+            mainReddit = new Reddit(); // Get a "reddit" instance.
+            web = new HtmlWeb();
+
+            modNames = new List<string>();
+            modSubsList = new List<List<string>>();
+            barNumber = 1;
+        }
+
+        private void buttonAnalyze_Click(object sender, EventArgs e)
+        {
+            if (!buttonAnalyze.Enabled)
+                return; // How did this even happen?
+
+            // Restore some values
+            modNames = new List<string>();
+            modSubsList = new List<List<string>>();
+            treeViewResults.Nodes.Clear();
+            treeViewSub.Nodes.Clear();
+
+            // Validate textBoxSubreddit text
+            mainSubreddit = textBoxSubreddit.Text;
+            // Remove superfluous info in subreddit
+            stripSubreddit();
+
+            // Re-add the /r/
+            string arrrr = "/r/";
+            mainSubreddit = arrrr + mainSubreddit;
+
+            // DUMMY, display a new subreddit
+            textBoxSubreddit.Text = mainSubreddit;
+
+            isValid = isValidSubreddit();
+
+            getMods(); // Just gets the name of the mods.
+
+            // We should do this in a thread of some sort with a progress bar.
+            progressBarMain.Minimum = 1;
+            progressBarMain.Maximum = moderators.Count();
+
+            Thread thread = new Thread(fillList);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void stripSubreddit()
+        {
+            // Check mainSubreddit for slashes before
+            string parsedSubreddit;
+            if (mainSubreddit.Contains("r/")) // It's a link, not just the sub.  Parse up to the / after /r/
+            {
+                int loc = mainSubreddit.IndexOf("/");
+                parsedSubreddit = mainSubreddit.Substring(loc + 3);
+
+                mainSubreddit = parsedSubreddit;
+
+                stripSubreddit(); // might need more parsing, recurse until we've stripped up to the r/
+            }
+            else if(mainSubreddit.Contains("/")) // if we've made it this far, look for the final /
+            {
+                // Strip off everything but what's before the slash
+                int loc = mainSubreddit.IndexOf("/");
+                parsedSubreddit = mainSubreddit.Substring(0, loc);
+
+                mainSubreddit = parsedSubreddit;
+
+                stripSubreddit(); // recurse just in case.
+            }
+        }
+
+        private bool isValidSubreddit()
+        {
+            return true; // dummy value until we have a way to validate subs
+        }
+
+        private void getMods()
+        {
+            // Let's see if we can get mods
+            var subreddit = mainReddit.GetSubreddit(mainSubreddit);
+
+            moderators = subreddit.Moderators;
+        }
+
+
+        // BEGIN THREADING!
+        private void fillList()
+        {
+            // Lock out everything, reset progressbar
+            barNumber = 1;
+
+            buttonAnalyze.Invoke((MethodInvoker)(() => buttonAnalyze.Enabled = false));
+
+            foreach (ModeratorUser mod in moderators) // For each moderator that we got from reddit...
+            {
+                string name = mod.Name; // Assign a name.
+
+                if (name == "AutoModerator") // Skip AutoModerator, it's useless and takes forever
+                    continue;
+
+                List<string> subreddits = getSubList(name); // Get a List<string> with their subreddits
+
+                // We have their name and a list of subreddits, add a tree node
+                TreeNode node = new TreeNode(name); // ID is the mod name
+                node.Name = name; // for manual node finding
+                foreach (string sub in subreddits) // For each subreddit...
+                {
+                    TreeNode subredditNode = new TreeNode(sub); // ID is the sub name
+                    subredditNode.Name = sub; // For manual node finding
+                    node.Nodes.Add(subredditNode); // Add the node
+
+                    // Update treeViewSub
+                    TreeNode[] itemNode = treeViewSub.Nodes.Find(sub, false); // Do we have this sub in the tree?
+
+                    if (itemNode.Length == 0) // Not found, add it
+                    {
+                        TreeNode newNode = new TreeNode(sub);
+                        newNode.Name = sub;
+                        newNode.Nodes.Add(name);
+                        // ADD
+                        treeViewSub.Invoke((MethodInvoker)(() => treeViewSub.Nodes.Add(newNode)));
+                    }
+                    else // Found, update it
+                    {
+                        treeViewSub.Invoke((MethodInvoker)(() => itemNode[0].Nodes.Add(name)));
+                    }
+                    
+                }
+
+                // ADD
+                treeViewResults.Invoke((MethodInvoker)(() => treeViewResults.Nodes.Add(node)));
+
+                Update1(barNumber); // Move the progress bar
+                barNumber++; // Incriment the progress bar
+            }
+
+            // Final Update
+            updateSubredditsByCount();
+
+            // Unlock
+            buttonAnalyze.Invoke((MethodInvoker)(() => buttonAnalyze.Enabled = true));
+        }
+
+        private List<string> getSubList(string modName)
+        {
+            List<string> retList = new List<string>();
+
+            string Url = "http://www.reddit.com/user/" + modName;
+            HtmlAgilityPack.HtmlDocument doc = web.Load(Url);
+            HtmlNode sideModList = doc.DocumentNode.SelectSingleNode("//ul[@id='side-mod-list']");
+
+            // Check that the user isn't banned...
+            bool banned = false;
+            if (sideModList == null)
+                banned = true;
+
+            if (!banned)
+            {
+                // We have the node, get the subnodes
+                HtmlNodeCollection subNodes = sideModList.ChildNodes;
+
+                foreach (HtmlNode node in subNodes)
+                {
+                    HtmlNode innerNode = node.FirstChild;
+                    // innerNode.InnerText = the sub!
+                    retList.Add(innerNode.InnerText);
+                }
+            }
+            else
+            {
+                retList.Add("USER IS BANNED");
+            }
+
+            return retList;
+        }
+
+        private void updateSubredditsByCount()
+        {
+            // treeViewSub has a handful of stuff in it.  We want them to be ordered by number of children.
+            TreeNodeCollection allNodes = treeViewSub.Nodes;
+            Dictionary<TreeNode, int> nodeDict = new Dictionary<TreeNode, int>();
+            List<TreeNode> finalList = new List<TreeNode>();
+
+            // First fill
+            foreach (TreeNode node in allNodes)
+            {
+                int count = node.GetNodeCount(false);
+                nodeDict[node] = count;
+            }
+
+            // Now that it's filled with counts, we need the highest count.
+            while(nodeDict.Count > 0)
+            {
+                int top = 0;
+                TreeNode topNode = null;
+
+                foreach (KeyValuePair<TreeNode, int> pair in nodeDict)
+                {
+                    if (pair.Value > top)
+                    {
+                        top = pair.Value;
+                        topNode = pair.Key;
+                    }
+                }
+
+                if (topNode != null) // found one, remove it
+                {
+                    treeViewSub.Invoke((MethodInvoker)(() => topNode.Text = topNode.Text + " (" + topNode.Nodes.Count + ")"));
+                    //topNode.Text = topNode.Text + " (" + topNode.Nodes.Count + ")";
+                    finalList.Add(topNode);
+                    nodeDict.Remove(topNode);
+                }
+            }
+
+            // Now we have a "finalList" that's filled.  Update treeviewSub with new values
+            treeViewSub.Invoke((MethodInvoker)(() => treeViewSub.Nodes.Clear())); // clear
+            
+            for(int i = 0; i < finalList.Count; i++)
+            {
+                treeViewSub.Invoke((MethodInvoker)(() => treeViewSub.Nodes.Add(finalList[i])));
+            }
+        }
+
+        public void Update1(int i)
+        {
+            if (InvokeRequired)
+            {
+                this.BeginInvoke(new Action<int>(Update1), new object[] { i });
+                return;
+            }
+
+            progressBarMain.Value = i;
+        }
+    }
+}
